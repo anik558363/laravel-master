ee310cb355fa1519a2ebe5c439ad132ff8560c4f
anik558363 <anik558363@gmail.com>
2025-11-17 16:13:54 +0600

    laravel-master project first part done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FrontendController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/', [FrontendController::class, 'index'])->name('frontend');
+Route::get('/resume', [FrontendController::class, 'resume'])->name('resume');
+Route::get('/projects', [FrontendController::class, 'projects'])->name('projects');
+Route::get('/contact', [FrontendController::class, 'contact'])->name('contact');
